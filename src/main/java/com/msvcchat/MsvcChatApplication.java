9048f1907728835e411aae8acc73eddb4c79a8ba
Raydberg Gabriel <raydbergg@gmail.com>
2025-08-27 21:55:34 -0500

    first commit

---
+++ src/main/java/com/msvcchat/MsvcChatApplication.java
@@ -0,0 +1,13 @@
+package com.msvcchat;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsvcChatApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MsvcChatApplication.class, args);
+    }
+
+}
