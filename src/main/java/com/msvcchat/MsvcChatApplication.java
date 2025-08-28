d9194e9e57c07b159454e583b759cbdc21ed07b5
Raydberg Gabriel <raydbergg@gmail.com>
2025-08-27 23:35:09 -0500

    configuration

--- src/main/java/com/msvcchat/MsvcChatApplication.java
+++ src/main/java/com/msvcchat/MsvcChatApplication.java
@@ -5,7 +5,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class MsvcChatApplication {
-
     public static void main(String[] args) {
         SpringApplication.run(MsvcChatApplication.class, args);
     }
---
+++ src/main/java/com/msvcchat/exceptions/ErrorResponse.java
@@ -0,0 +1,14 @@
+package com.msvcchat.exceptions;
+
+import lombok.*;
+
+import java.util.List;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class ErrorResponse {
+    private String errorCode;
+    private String errorMessage;
+    private List<String> details;
+}
