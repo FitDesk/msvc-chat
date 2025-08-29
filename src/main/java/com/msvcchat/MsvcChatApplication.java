99c24c73938b71c21ec28f8108e133afa817b086
Raydberg Gabriel <raydbergg@gmail.com>
2025-08-28 19:49:17 -0500

    configuration

--- src/main/java/com/msvcchat/MsvcChatApplication.java
+++ src/main/java/com/msvcchat/MsvcChatApplication.java
@@ -2,8 +2,9 @@ package com.msvcchat;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class})
 public class MsvcChatApplication {
     public static void main(String[] args) {
         SpringApplication.run(MsvcChatApplication.class, args);
